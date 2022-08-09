39d8d5815759c36089ef6c2b8583e80212afd09f
Biyyala Chandramouli <mouli150388@gmail.com>
2022-08-09 14:08:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    String TAG=MainActivity.class.getCanonicalName();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button b = (Button)findViewById(R.id.button);
+        b.setOnClickListener((View v) ->
+                {
+                    Log.d("Welcome","Welcome");
+                    testLambda1();
+                    testLambda2();
+                }
+        );
+    }
+
+
+    public void testLambda1(){
+
+        Log.d("TAG", "This is new test for Git: and it is fixed ");
+        Log.e(TAG, "testLambdaerror: " );
+        Log.i(TAG, "testLambdainfo: ");
+        Log.v(TAG, "verbose");
+        Log.w(TAG, "testLambdaw: ");
+        Log.wtf(TAG, "WTF");
+        Log.wtf(TAG, "WTF");
+
+    }
+    public void testLambda2(){
+        Log.d("TAG", "testLambda2: ");
+    }
+
+    public void testFunctionalInterfaceLambda(Person p){
+
+
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/Person.java
@@ -0,0 +1,12 @@
+package com.example.myapplication;
+
+public class Person {
+    Person(String name, int age, int id){
+        this.name = name;
+        this.age = age;
+        this.id = id;
+    }
+    String name;
+    int age;
+    int id;
+}
